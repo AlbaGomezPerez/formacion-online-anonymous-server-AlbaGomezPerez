800e41dd104780b4ca438ed7f90aef2065241341
Alba Gómez Pérez <albagope@gmail.com>
2019-11-06 14:10:13 +0100

    first lines in javaScript

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
 
 //query selector botón menú:
 //Al hacer click en menú, la función pone la clase "mainHidden" y quita la clase "infoHidden"
+'use strict';
+
+const menuButton = document.querySelector('.nav-trigger');
+const infoMenu = document.querySelector('.nav');
+const cancelButton = document.querySelector('.nav-close');
+console.log(menuButton);
+console.log(infoMenu);
+
+function openMenu() {
+    console.log('hola');
+    if (infoMenu.classList.contains('infoHidden')) {
+        infoMenu.classList.remove('infoHidden');
+    } else {
+        infoMenu.classList.add('infoHidden');
+    }
+
+}
+
+menuButton.addEventListener('click', openMenu);
+
+
+
+
+// const navTrigger  = document.getElementById("nav-trigger");
+// const nav = document.getElementById("nav");
+//
+// function openMenu(){
+//         ("nav").fadeToggle("slow");
+// };
+//
+// navTrigger.addEventListener('click', openMenu);
+
